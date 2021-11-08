43338f3094e5b75c16c90e622dc74e80d46715a3
speedytman <treycoker09@gmail.com>
2021-11-08 16:52:20 -0500

    WIP: wrapped the card buttons in a grid layout, started working on logic for assigning face value to cards.

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -12,11 +13,14 @@ import androidx.core.content.ContextCompat;
 public class MainActivity extends AppCompatActivity {
 
     private MemoryGame mGame;
+    private GridLayout mCardGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCardGrid = findViewById(R.id.card_grid);
 
         mGame = new MemoryGame();
         startGame();
@@ -24,9 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void startGame() {
         mGame.newGame();
+        setFaceValues();
     }
+
+    private void setFaceValues() {
+
+    }
+
 
     public void onNewGameClick(View view){
         startGame();
     }
+
+    public void onCardClick(View view) {
+        int buttonIndex = mCardGrid.indexOfChild(view);
+        int row = buttonIndex / MemoryGame.GRID_ROW;
+        int col = buttonIndex % MemoryGame.GRID_COL;
+
+        mGame.selectCard(row, col);
+        setFaceValues();
+
+        if(mGame.isGameOver()){
+            //Toast.makeText(this, R.string.congrats, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
--- app/src/main/java/com/example/memorygame/MemoryGame.java
+++ app/src/main/java/com/example/memorygame/MemoryGame.java
@@ -1,19 +1,83 @@
 package com.example.memorygame;
 
-public class MemoryGame {
-    public MemoryGame() {
+import android.util.Log;
 
+import java.util.Random;
+
+public class MemoryGame {
+    public static final int GRID_COL = 3;
+    public static final int GRID_ROW = 4;
+
+    int faceOneCount;
+    int faceTwoCount;
+    int faceThreeCount;
+    int faceFourCount;
+    int faceFiveCount;
+    int faceSixCount;
+
+    private final int[][] mCardGrid;
+    public MemoryGame() {
+        mCardGrid = new int[GRID_ROW][GRID_COL];
     }
 
     public void newGame() {
+        int faceOneCount = 0;
+        int faceTwoCount = 0;
+        int faceThreeCount = 0;
+        int faceFourCount = 0;
+        int faceFiveCount = 0;
+        int faceSixCount = 0;
+        for(int row = 0; row < GRID_ROW; row++){
+            for(int col = 0; col < GRID_COL; col++){
+                assignFace(row, col, faceOneCount, faceTwoCount, faceThreeCount, faceFourCount, faceFiveCount, faceSixCount);
+            }
+        }
+    }
 
+    public void assignFace(int row, int col, int faceOneCount, int faceTwoCount, int faceThreeCount, int faceFourCount, int faceFiveCount, int faceSixCount) {
+        Random randomNumGenerator = new Random();
+
+        int suggestedFace = randomNumGenerator.nextInt(5);
+        if(suggestedFace == 0 && faceOneCount < 2){
+            mCardGrid[row][col] = suggestedFace;
+            Log.d("stuff", "" + suggestedFace);
+            faceOneCount++;
+        }
+        if(suggestedFace == 0 && faceTwoCount < 2){
+            mCardGrid[row][col] = suggestedFace;
+            Log.d("stuff", "" + suggestedFace);
+            faceTwoCount++;
+        }
+        if(suggestedFace == 0 && faceThreeCount < 2){
+            mCardGrid[row][col] = suggestedFace;
+            Log.d("stuff", "" + suggestedFace);
+            faceThreeCount++;
+        }
+        if(suggestedFace == 0 && faceFourCount < 2){
+            mCardGrid[row][col] = suggestedFace;
+            Log.d("stuff", "" + suggestedFace);
+            faceFourCount++;
+        }
+        if(suggestedFace == 0 && faceFiveCount < 2){
+            mCardGrid[row][col] = suggestedFace;
+            Log.d("stuff", "" + suggestedFace);
+            faceFiveCount++;
+        }
+        if(suggestedFace == 0 && faceSixCount < 2){
+            mCardGrid[row][col] = suggestedFace;
+            Log.d("stuff", "" + suggestedFace);
+            faceSixCount++;
+        }
+        else {
+            assignFace(row, col, faceOneCount, faceTwoCount, faceThreeCount, faceFourCount, faceFiveCount, faceSixCount);
+        }
     }
 
     public boolean doCardsMatch() {
         return false;
     }
 
-    public void selectCard() {
+    public void selectCard(int row, int col) {
 
     }
 
