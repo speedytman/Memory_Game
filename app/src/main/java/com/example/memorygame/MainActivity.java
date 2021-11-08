5c047421236d655d6f3755ec5e211a9e1f7a3331
speedytman <treycoker09@gmail.com>
2021-11-08 12:36:04 -0500

    Added new game button, created MemoryGame class and added blank methods for logic.

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.memorygame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MemoryGame mGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGame = new MemoryGame();
+        startGame();
+    }
+
+    public void startGame() {
+        mGame.newGame();
+    }
+
+    public void onNewGameClick(View view){
+        startGame();
     }
 }
---
+++ app/src/main/java/com/example/memorygame/MemoryGame.java
@@ -0,0 +1,23 @@
+package com.example.memorygame;
+
+public class MemoryGame {
+    public MemoryGame() {
+
+    }
+
+    public void newGame() {
+
+    }
+
+    public boolean doCardsMatch() {
+        return false;
+    }
+
+    public void selectCard() {
+
+    }
+
+    public boolean isGameOver() {
+        return false;
+    }
+}
