3355eca3345ee63a8282ae86f7cd0278e09bed10
jcart48 <jayjaydscarter@gmail.com>
2021-12-05 07:56:52 -0500

    The switch "works" , but it doesn't stop the music on the main activity page. Lines 25-26.

--- app/src/main/java/com/example/memorygame/SettingsActivity.java
+++ app/src/main/java/com/example/memorygame/SettingsActivity.java
@@ -3,20 +3,29 @@ package com.example.memorygame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
+    Switch music_off;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        music_off = (Switch) findViewById(R.id.music_off);
     }
 
     public void onBackHomeClick (View view) {
         Intent intent = new Intent(SettingsActivity.this, HomeActivity.class);
         startActivity(intent);
+    }
+     public void onMusicSwitch (View view){
+         // if (music_off.isChecked())
+           //  music_off.stop();
+
+
     }
 
 
